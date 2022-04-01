7138a6a85b8bc3ebacbf94b7b65879eec2c76970
kimjinmin4535 <kimjinmin4535@gmail.com>
2022-04-01 14:14:08 +0900

    add Good

--- Hello.java
+++ Hello.java
@@ -6,6 +6,8 @@ public class Hello{
 		System.out.println("Register");
        		System.out.println("what is Git?");
 		System.out.println("That's DVCS!");
+		System.out.println("Good Afternoon!");
+
 	}
 }
 
