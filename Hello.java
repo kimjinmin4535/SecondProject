47fbf7c94aaa2f7aaefec9743f80d4f19a4e7a41
kimjinmin4535 <102012095+kimjinmin4535@users.noreply.github.com>
2022-04-01 14:24:37 +0900

    add Hello3

--- Hello.java
+++ Hello.java
@@ -9,7 +9,8 @@ public class Hello{
 		System.out.println("Register");
        		System.out.println("what is Git?");
 		System.out.println("That's DVCS!");
-		System.out.println("Hello Git2;);
+		System.out.println("Hello Git2!");
+		System.out.println("Hello Git3!");
 	}
 }
 
